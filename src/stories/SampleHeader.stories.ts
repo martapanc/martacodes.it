a662907043af45fe7897a5add560513e99667a5f
marta.pancaldi <marta.pancaldi@resourcify.de>
2023-06-12 22:38:52 +0200

    docs: add Header docs

---
+++ src/stories/SampleHeader.stories.ts
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { SampleHeader } from './SampleHeader';
+
+const meta: Meta<typeof SampleHeader> = {
+  title: 'Example/SampleHeader',
+  component: SampleHeader,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'fullscreen',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof SampleHeader>;
+
+export const LoggedIn: Story = {
+  args: {
+    user: {
+      name: 'Jane Doe',
+    },
+  },
+};
+
+export const LoggedOut: Story = {};
