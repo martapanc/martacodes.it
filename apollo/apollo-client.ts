0cf0cc9e509503b18131d6dfc3e9919ed43ea5f9
emils.kadikis <emils.kadikis@resourcify.de>
2023-09-06 19:05:55 +0200

    chore: add connectToDevTools option

--- apollo/apollo-client.ts
+++ apollo/apollo-client.ts
@@ -3,6 +3,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 const apolloClient = new ApolloClient({
   uri: process.env.GRAPHQL_URL,
   cache: new InMemoryCache(),
+  connectToDevTools: true,
   defaultOptions: {
     watchQuery: {
       fetchPolicy: 'no-cache',
