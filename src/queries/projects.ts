a303e32e5d47bbc99ea94edebbbd3fbe5b796025
martapanc <marta.panc@gmail.com>
2023-09-12 23:17:55 +0200

    fix: add error handling for retrieving projects

--- src/queries/projects.ts
+++ src/queries/projects.ts
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client';
+import { ApolloError, gql } from '@apollo/client';
 
 import { flattenToArray } from '@/lib/graphqlUtils';
 
@@ -7,27 +7,38 @@ import apolloClient from '../../apollo/apollo-client';
 import { Project, RawProject } from '@/types/Project';
 
 export async function queryProjects() {
-  const { data } = await apolloClient.query({ query: projectsQuery });
+  try {
+    const { data } = await apolloClient.query({ query: projectsQuery });
 
-  const result: RawProject[] = flattenToArray<RawProject>(data.projects);
-  const projects: Project[] = [];
+    const result: RawProject[] = flattenToArray<RawProject>(data.projects);
+    const projects: Project[] = [];
 
-  result.map((entry) => {
-    const project: Project = {
-      id: entry.id,
-      title: entry.title,
-      image: entry.image,
-      shortDescription: entry.shortDescription,
-      longDescription: entry.longDescription,
-      tools: entry.tools.split(','),
-      date: entry.date,
-      tags: entry.tags.split(','),
-      links: entry.links,
-    };
-    projects.push(project);
-  });
+    result.map((entry) => {
+      const project: Project = {
+        id: entry.id,
+        title: entry.title,
+        image: entry.image,
+        shortDescription: entry.shortDescription,
+        longDescription: entry.longDescription,
+        tools: entry.tools.split(','),
+        date: entry.date,
+        tags: entry.tags.split(','),
+        links: entry.links,
+      };
+      projects.push(project);
+    });
 
-  return projects;
+    return projects;
+  } catch (error) {
+    if (error instanceof ApolloError) {
+      // eslint-disable-next-line no-console
+      console.error('Apollo Client Error:', error.message);
+    } else {
+      // eslint-disable-next-line no-console
+      console.error('An unexpected error occurred:', error);
+    }
+    return [];
+  }
 }
 
 const projectsQuery = gql`
