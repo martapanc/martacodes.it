6ecb4431de060ed3bc25d086e99ee2dee1d29fa4
martapanc <marta.panc@gmail.com>
2023-10-07 21:46:33 +0200

    chore: undo unneeded changes

--- src/pages/api/skills.ts
+++ src/pages/api/skills.ts
@@ -9,8 +9,6 @@ import { Skill } from '@/types/Skill';
 const skillsApi = async (req: NextApiRequest, res: NextApiResponse) => {
   const locale = getLocaleFromCookies(req);
 
-  console.log(locale);
-
   const skills: Skill[] = await querySkills(locale);
 
   res.status(200).json(skills);
