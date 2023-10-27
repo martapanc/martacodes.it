e137171fdd41d3a419a4e66321d8e75309681ff6
martapanc <marta.panc@gmail.com>
2023-10-27 20:46:18 +0200

    chore: reduce revalidate time

--- apollo/apollo-client.ts
+++ apollo/apollo-client.ts
@@ -19,12 +19,12 @@ export default apolloClient;
 
 export const context_short = {
   fetchOptions: {
-    next: { revalidate: 86400 },
+    next: { revalidate: 3600 },
   },
 };
 
 export const context_long = {
   fetchOptions: {
-    next: { revalidate: 604800 },
+    next: { revalidate: 86400 },
   },
 };
