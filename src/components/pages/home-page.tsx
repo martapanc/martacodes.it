44d5ca10a9eb836f970ab4e1897cc28a2c85dac1
martapanc <marta.panc@gmail.com>
2024-03-14 13:20:04 +0100

    feat: improve Summary section

--- src/components/pages/home-page.tsx
+++ src/components/pages/home-page.tsx
@@ -14,7 +14,7 @@ export default function HomePage({ homePage, codeSnippets }: HomePageProps) {
   return (
     <section className='dark:bg-dark bg-white'>
       <div className='dark:bg-gradient-linear-dark bg-gradient-linear-light'>
-        <div className='layout relative flex flex-col py-16 md:pt-24 pb-16'>
+        <div className='layout relative flex flex-col py-16 md:pt-24 pb-5 md:pb-16'>
           <Intro greeting={homePage.greeting} codeSnippets={codeSnippets} />
         </div>
       </div>
